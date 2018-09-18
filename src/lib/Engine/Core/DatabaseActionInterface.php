07da2ca45bbedc7fd50205237f8c6b67d3fc3ee4
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-09-18 18:28:54 +0300

    Action interfaces

---
+++ src/lib/Engine/Core/DatabaseActionInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package   AkeebaReplace
+ * @copyright Copyright (c)2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @author    Nicholas K. Dionysopoulos
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL v3 or later
+ *
+ */
+
+namespace Akeeba\Replace\Engine\Core;
+
+
+use Akeeba\Replace\Engine\Database\Metadata\Database;
+use Akeeba\Replace\Engine\Core\Response\SQL;
+
+interface DatabaseActionInterface
+{
+	/**
+	 * Take a database connection and figure out if we need to run database-level DDL queries.
+	 *
+	 * @param   Database $db The database definition we are processing
+	 *
+	 * @return  SQL
+	 */
+	public function processDatabase(Database $db);
+}
