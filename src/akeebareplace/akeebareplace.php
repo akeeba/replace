b5f7bd86857e21f0bfb0aaafef653b550aa282df
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-09-26 14:43:11 +0300

    Skeleton WP plugin

---
+++ src/akeebareplace/akeebareplace.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Akeeba Replace
+Plugin URI: https://www.akeebabackup.com
+Description: Database mass content replace
+Version: 1.0.0
+Author: Akeeba Ltd
+Author URI: https://www.akeebabackup.com
+License: GPLv3
+*/
+
+/**
+ * @package   AkeebaReplace
+ * @copyright Copyright (c)2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @author    Nicholas K. Dionysopoulos
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL v3 or later
+ *
+ */
