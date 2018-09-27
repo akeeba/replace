51c75ef8c217dcabcac2a1c422c90cf10f65eca3
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-09-27 13:43:54 +0300

    Project relinker support

---
+++ build/templates/relink-wp.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package   AkeebaReplace
+ * @copyright Copyright (c)2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @author    Nicholas K. Dionysopoulos
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL v3 or later
+ *
+ */
+
+$hardlink_files = [
+];
+
+$symlink_files = [
+];
+
+$symlink_folders = [
+	# The plugin itself
+	'src/akeebareplace' => 'wp-content/plugins/akeebareplace'
+];
