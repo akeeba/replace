43d0e5491f0f7745da8323fb072a1d8a03f957ba
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-09-29 09:54:37 +0300

    Link to FEF

--- build/templates/link.php
+++ build/templates/link.php
@@ -6,5 +6,11 @@ $symlink_files = array(
 );
 
 $symlink_folders = array(
-	'src/lib' => 'src/akeebareplace/includes/lib'
+	'src/lib' => 'src/akeebareplace/includes/lib',
+
+	# Akeeba FEF
+	'../fef/wp-css'		=> 'src/akeebareplace/fef/css',
+	'../fef/fonts'		=> 'src/akeebareplace/fef/fonts',
+	'../fef/images'		=> 'src/akeebareplace/fef/images',
+	'../fef/js'			=> 'src/akeebareplace/fef/js',
 );
