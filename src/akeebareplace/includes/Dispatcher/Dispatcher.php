c6923e0b224ecd7f7690609a56200819d2307365
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-10-05 12:42:54 +0300

    Replace jQuery with vanilla JavaScript
    Import the System library

--- src/akeebareplace/includes/Dispatcher/Dispatcher.php
+++ src/akeebareplace/includes/Dispatcher/Dispatcher.php
@@ -9,9 +9,14 @@
 
 namespace Akeeba\Replace\WordPress\Dispatcher;
 
+use Akeeba\Replace\WordPress\Helper\Application;
 use Akeeba\Replace\WordPress\MVC\Dispatcher\Dispatcher as AbstractDispatcher;
 
 class Dispatcher extends AbstractDispatcher
 {
-
+	protected function onBeforeRoute($view, $task)
+	{
+		wp_enqueue_script('akeebareplace-modal', plugins_url('/js/modal.js', AKEEBA_REPLACE_SELF), [], Application::getMediaVersion());
+		wp_enqueue_script('akeebareplace-system', plugins_url('/js/system.js', AKEEBA_REPLACE_SELF), ['akeebareplace-modal'], Application::getMediaVersion());
+	}
 }
