c90c45b3b246a7f81f6c20606ed54354bf8b1a95
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-17 14:38:17 +0200

    Fixed PHP 7.4 notice

--- src/lib/Database/Driver.php
+++ src/lib/Database/Driver.php
@@ -1320,7 +1320,7 @@ abstract class Driver implements DatabaseInterface
 					break;
 				}
 				$l = $k - 1;
-				while ($l >= 0 && $sql{$l} == '\\')
+				while ($l >= 0 && $sql[$l] == '\\')
 				{
 					$l--;
 					$escaped = !$escaped;
